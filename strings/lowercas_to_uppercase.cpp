dcd645d544711981b0c9fdff77ec11d0d77cd704
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-08 00:11:16 +0530

    update

---
+++ strings/lowercas_to_uppercase.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s_1;
+    cout << " enter the string 1" << endl;
+    getline(cin, s_1);
+    cout << " the string is :" << (s_1) << endl;
+
+    return 0;
+}
