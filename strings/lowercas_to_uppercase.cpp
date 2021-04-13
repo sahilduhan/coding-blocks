c1f096c8bf57facd45a36ad46b13f47754666f0c
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-13 15:45:08 +0530

    git update

--- strings/lowercas_to_uppercase.cpp
+++ strings/lowercas_to_uppercase.cpp
@@ -5,7 +5,14 @@ int main()
     string s_1;
     cout << " enter the string 1" << endl;
     getline(cin, s_1);
+    int n = s_1.length();
     cout << " the string is :" << (s_1) << endl;
 
+    cout << " the length is :" << n << endl;
+
+    // cout << " the new string is :" << s_1.insert(3, "sahil")
+    //      << endl;
+    string s_2 = s_1.substr(4, 3);
+    cout << s_2 << endl;
     return 0;
 }
