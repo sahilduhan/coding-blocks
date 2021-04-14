de92f24c1d2dafb18b27cd18244449cb891b6ba2
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-14 21:54:06 +0530

    uu[date

--- 2_d_array/basic_2d_array.cpp
+++ 2_d_array/basic_2d_array.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 int main()
 {
-    int row = 5;
-    int col = 4;
-    int val = 0;
-    int arr[row][col];
-    for (int i = 0; i < row; i++)
+    // int row = 5;
+    // int col = 4;
+    // int val = 0;
+    // int arr[row][col];
+    // for (int i = 0; i < row; i++)
+    // {
+
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         arr[row][col] = val;
+    //         val = val + 1;
+    //         cout << arr[row][col] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    int n = 696969;
+    for (int i = 0; i < n; i++)
     {
-
-        for (int j = 0; j < col; j++)
-        {
-            arr[row][col] = val;
-            val = val + 1;
-            cout << arr[row][col] << " ";
-        }
-        cout << endl;
+        cout << " ruk jao janwaro ";
     }
-
     return 0;
 }
