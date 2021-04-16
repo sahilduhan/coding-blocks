ae6a40736773fa88c9c06f092ccdeba3f158cac2
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-16 13:50:25 +0530

    update

--- bitwise_operators/decimal_to_binary.cpp
+++ bitwise_operators/decimal_to_binary.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
+    long long int num = 142;
+    vector<long long int> bit_vector;
+    while (num > 0)
+    {
+        bit_vector.push_back(num % 2);
+        num = num / 2;
+    }
+    for (int i = bit_vector.size() - 1; i > 0; i--)
+    {
+        cout << bit_vector.at(i);
+    }
 
     return 0;
 }
