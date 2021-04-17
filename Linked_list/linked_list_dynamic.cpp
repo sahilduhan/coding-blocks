6e77e035d62dcc8d8be5a2c2532b30ebc9219242
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-17 15:21:35 +0530

    updaet

--- Linked_list/linked_list_dynamic.cpp
+++ Linked_list/linked_list_dynamic.cpp
@@ -13,11 +13,14 @@ public:
 };
 int main()
 {
-    node n1(1);
-    node *head = &n1;
-    node n2(2);
+    node n1(10);
+    node n2(20);
     n1.next = &n2;
-    cout<<head->data<<" ";
-    cout << n1.data << " " << n2.data << " ";
+    cout << n1.data << " " << n2.data << endl;
+    node *n3 = new node(30);
+    node *n4 = new node(40);
+    // node *head = &n3;
+    n3->next = n4;
+
     return 0;
 }
