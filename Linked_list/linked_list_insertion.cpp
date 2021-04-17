ef11877abb8e58f5cbfa71c21662cee8e12f1062
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-17 14:12:49 +0530

    update

--- Linked_list/linked_list_insertion.cpp
+++ Linked_list/linked_list_insertion.cpp
@@ -11,15 +11,31 @@ public:
         next = NULL;
     }
 };
-void insert_element(node *head, int data)
+//passing a pointer variable by reffrence
+void insert_element(node *&head, int data)
 {
     node *n = new node(data);
     n->next = head;
     head = n;
 }
+void print(node *head)
+{
+
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
     node *head = NULL;
+    insert_element(head, 5);
+    insert_element(head, 4);
+    insert_element(head, 3);
+    print(head);
 
     return 0;
 }
