c6987352cf7313e62d8bceb8e4b6dff361a781f2
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-18 13:23:20 +0530

    update

---
+++ Linked_list/inserting_better.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+node *take_input()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    node *head = take_input();
+    print(head);
+
+    return 0;
+}
