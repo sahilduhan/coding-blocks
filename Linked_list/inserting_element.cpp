4aed6d8d19bbaf8f27cfeb1a538f951ccdfff595
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-18 13:47:51 +0530

    update

---
+++ Linked_list/inserting_element.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+node *take_input()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+void insert_node(node *head, int i, int data)
+{
+    node *newnode = new node(data);
+}
+int main()
+{
+
+    return 0;
+}
