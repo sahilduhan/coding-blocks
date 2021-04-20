880b81d57e72111e36ffea2bb0f0a5da1ab23a64
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-20 23:39:30 +0530

    update

---
+++ linked_list_again/linked_list_2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+    node*head;
+    int data;
+};
+void insert(int x)
+{
+    
+
+
+}
+void print()
+{
+
+}
+int main()
+{
+
+
+
+    return 0;
+}
