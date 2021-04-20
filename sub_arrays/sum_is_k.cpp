aae415e2e34d9caf0061bf3942c119913960e3ad
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-20 22:23:38 +0530

    updaet

--- sub_arrays/sum_is_k.cpp
+++ sub_arrays/sum_is_k.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int key = 5;
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int arr[] = {};
     int size = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < size; i++)
     {
