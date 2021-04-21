4b2475e6bacb40f45ed959636e4e4723501e2111
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-21 23:24:15 +0530

    update

---
+++ linked_list_again/printing._ist.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+int input(int x)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
