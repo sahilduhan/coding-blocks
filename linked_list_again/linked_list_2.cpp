fa9cc2ff25a1fe234f35134a026ec419cf87c98e
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-21 17:08:41 +0530

    update

--- linked_list_again/linked_list_2.cpp
+++ linked_list_again/linked_list_2.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 class node
 {
-    node*head;
+public:
     int data;
+    node *next;
 };
+node *head;
 void insert(int x)
 {
     
-
-
 }
 void print()
 {
-
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
 }
 int main()
 {
-
-
 
     return 0;
 }
