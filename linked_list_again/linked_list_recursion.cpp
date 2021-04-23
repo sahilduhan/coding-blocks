51f9bce68939c5bf52fcc418514a47c3c9c8df78
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-24 00:12:34 +0530

    update

--- linked_list_again/linked_list_recursion.cpp
+++ linked_list_again/linked_list_recursion.cpp
@@ -11,7 +11,29 @@ public:
         next = NULL;
     }
 };
-
+node *newnode(int data)
+{
+    node *new_node = new node(data);
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+node *take_input(node *head, int data)
+{
+    if (head == NULL)
+        return newnode(data);
+    else
+        head->next = take_input(head->next, data);
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
 int main()
 {
 
