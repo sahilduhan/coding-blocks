f18b4546a536e9150e4285453252786810fc74b2
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-26 00:59:36 +0530

    update

---
+++ linked_list_again/reverse_linked_recursion.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+node *take_input()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *new_node = new node(data);
+        if (head == NULL)
+        {
+            head = new_node;
+            tail = new_node;
+        }
+        else
+        {
+            tail->next = new_node;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+void merge_sort(node *head)
+{
+}
+int manin()
+{
+    node *head = take_input();
+    print(head);
+
+    return 0;
+}
