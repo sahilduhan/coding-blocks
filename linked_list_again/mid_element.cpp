053fb2e68b5653f78983c339856ec87781513c45
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-26 00:01:59 +0530

    update

--- linked_list_again/mid_element.cpp
+++ linked_list_again/mid_element.cpp
@@ -1,7 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+node *take_input()
+{
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while (data != -1)
+    {
+        node *newnode = new node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+void print_data(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+void length(node *head)
+{
+    int count = 0;
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    cout << endl
+         << " the length is ";
+    cout << count << " ";
+}
+void mid_element(node *head, int slow, int fast)
+{
+}
+
 int main()
 {
-
+    node *head = take_input();
+    print_data(head);
+    length(head);
     return 0;
 }
