84e5af1fa4cd9d25919ea0c8b8125079df5a1e36
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-28 23:20:51 +0530

    udpaet

--- stacks_and_ques/stack_using_array_2.cpp
+++ stacks_and_ques/stack_using_array_2.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Stack_using_array
+{
+    int *data;
+    int next_index;
+    int capacity;
+
+public:
+    Stack_using_array()
+    {
+        data = new int();
+        next_index = 0;
+        capacity = 5;  
+    }
+};
 int main()
 {
-    
 
     return 0;
 }
