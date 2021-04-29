7be6965c8632aeb51cb75e9eabf28e4620524300
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-29 23:33:48 +0530

    update

---
+++ stacks_and_ques/template_type.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+class pair
+{
+    T x;
+    T y;
+
+public:
+    void set(T x)
+    {
+        this - x = x;
+    }
+    void get()
+    {
+        return x;
+    }
+    void setY(T y)
+    {
+        this->y = y;
+    }
+    void getY()
+    {
+        return y;
+    }
+};
+int main()
+{
+
+    return 0;
+}
