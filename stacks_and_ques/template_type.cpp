093abb79da437b2e18d3e56c4359d25dc50a7317
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-29 23:43:41 +0530

    update

--- stacks_and_ques/template_type.cpp
+++ stacks_and_ques/template_type.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 template <typename T>
-class pair
+class _Pair
 {
     T x;
     T y;
 
 public:
-    void set(T x)
+    void setX(T x)
     {
         this - x = x;
     }
-    void get()
+    void getX()
     {
         return x;
     }
@@ -26,6 +26,10 @@ public:
 };
 int main()
 {
+    _Pair<int> p1;
+    p1.setX(10);
+    p1.getX();
+    _Pair<int> p2;
 
     return 0;
 }
