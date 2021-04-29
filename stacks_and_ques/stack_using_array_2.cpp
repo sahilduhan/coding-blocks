cb301a215a48c4fdfed551ee6b0ef0ee4bd8b2a5
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-29 23:33:57 +0530

    update

--- stacks_and_ques/stack_using_array_2.cpp
+++ stacks_and_ques/stack_using_array_2.cpp
@@ -3,27 +3,27 @@ using namespace std;
 class Stack_using_array
 {
     int *data;
-    int next_index;
+    int Next_Index;
     int capacity;
 
 public:
     Stack_using_array()
     {
-        data = new int(5);
-        next_index = 0;
+        data = new int[5];
+        Next_Index = 0;
         capacity = 5;
     }
     int size()
     {
-        return next_index;
+        return Next_Index;
     }
     bool is_empty()
     {
-        return next_index == 0;
+        return Next_Index == 0;
     }
     void push(int x)
     {
-        if (next_index == capacity)
+        if (Next_Index == capacity)
         {
             int *new_data = new int[2 * capacity];
             for (int i = 0; i < capacity; i++)
@@ -35,17 +35,17 @@ public:
             data = new_data;
         }
 
-        data[next_index] = x;
-        next_index++;
+        data[Next_Index] = x;
+        Next_Index++;
     }
     int pop()
     {
-        next_index--;
-        return data[next_index];
+        Next_Index--;
+        return data[Next_Index];
     }
     int top()
     {
-        return data[next_index - 1];
+        return data[Next_Index - 1];
         if (is_empty())
         {
             cout << " stack is empty " << endl;
@@ -55,6 +55,14 @@ public:
 };
 int main()
 {
-
+    Stack_using_array s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.push(60);
+    s.top();
+    s.size();
     return 0;
 }
