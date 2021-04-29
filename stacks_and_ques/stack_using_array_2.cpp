fc88dc0705fa5e8698b3c02cc65051bf8b4093b6
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-04-29 23:15:54 +0530

    update

--- stacks_and_ques/stack_using_array_2.cpp
+++ stacks_and_ques/stack_using_array_2.cpp
@@ -9,9 +9,48 @@ class Stack_using_array
 public:
     Stack_using_array()
     {
-        data = new int();
+        data = new int(5);
         next_index = 0;
-        capacity = 5;  
+        capacity = 5;
+    }
+    int size()
+    {
+        return next_index;
+    }
+    bool is_empty()
+    {
+        return next_index == 0;
+    }
+    void push(int x)
+    {
+        if (next_index == capacity)
+        {
+            int *new_data = new int[2 * capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                new_data[i] = data[i];
+            }
+            capacity *= 2;
+            delete[] data;
+            data = new_data;
+        }
+
+        data[next_index] = x;
+        next_index++;
+    }
+    int pop()
+    {
+        next_index--;
+        return data[next_index];
+    }
+    int top()
+    {
+        return data[next_index - 1];
+        if (is_empty())
+        {
+            cout << " stack is empty " << endl;
+        }
+        return INT_MIN;
     }
 };
 int main()
