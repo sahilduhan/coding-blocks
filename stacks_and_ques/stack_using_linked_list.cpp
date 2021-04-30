5d7d3fb0045890768f52e953fb08ee18494c7973
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-01 01:08:35 +0530

    update

---
+++ stacks_and_ques/stack_using_linked_list.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+    int data;
+    int next_index;
+    node(int data)
+    {
+        this->data = data;
+        next_index = NULL;
+    }
+};
+node *head;
+
+int main()
+{
+
+    return 0;
+}
