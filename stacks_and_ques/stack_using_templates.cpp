ab271f120976f2de6242868f16241dec8a664fd9
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-01 00:10:07 +0530

    update

---
+++ stacks_and_ques/stack_using_templates.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+class Stack_using_array
+{
+    int *data;
+    int next_index;
+    int capacity;
+
+public:
+    Stack_using_array()
+    {
+        data = new T(4);
+        next_index=0;
+        capacity=4;
+    }
+};
+int main()
+{
+
+    return 0;
+}
