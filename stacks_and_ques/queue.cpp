397f0bcf62afbee0e40df45883c7d40abacdd987
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-02 16:37:27 +0530

    update

--- stacks_and_ques/queue.cpp
+++ stacks_and_ques/queue.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+template <typename T>
+class Queue_uing_array
+{
+    T *data;
+    int next_index;
+    int first_index;
+    int size;
+
+public:
+    Queue_uing_array(int s)
+    {
+        data = new T(s);
+        next_index = 0;
+        first_index = -1;
+        size = 0;
+    }
+};
 int main()
 {
-    
+
     return 0;
 }
