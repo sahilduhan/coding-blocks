da53e65899844594a5490c919cfb177e38c6a997
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-02 13:48:53 +0530

    git update

--- stacks_and_ques/stack_stl.cpp
+++ stacks_and_ques/stack_stl.cpp
@@ -4,6 +4,18 @@ int main()
 {
     stack<int> s1;
     s1.push(100);
+    s1.push(200);
+    s1.push(300);
+    s1.push(400);
+    s1.push(500);
+    s1.push(600);
+    s1.push(700);
+
     cout << s1.top() << endl;
+    s1.pop();
+    cout << s1.top() << endl;
+    cout << s1.size() << endl;
+    cout << s1.empty() << endl;
+
     return 0;
 }
