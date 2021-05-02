4ce3e52826ee92da80bdb56aeb405e217ee1ef7b
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-03 00:50:07 +0530

    update

---
+++ stacks_and_ques/queue_using_LL.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+class node
+{
+public:
+    T data;
+    node<T> *next;
+    node(T data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+template <typename T>
+class queue_using_ll
+{
+    node<T> *head;
+    node<T> *tail;
+    int size;
+
+public:
+    queue_using_ll
+    {
+        head = NULL;
+        tail = NULL;
+        size = 0;
+    }
+    int get_size()
+    {
+        return size;
+    }
+    bool is_empty()
+    {
+        return size == 0;
+    }
+    void enqueue(T element)
+    {
+        node<T> *new_node = new node<T>(element);
+        if (head == NULL)
+        {
+            tail->next = new_node;
+            tail = new_node;
+            size++;
+        }
+    }
+};
+int main()
+{
+
+    return 0;
+}
