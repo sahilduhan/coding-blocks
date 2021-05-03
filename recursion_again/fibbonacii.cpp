61f6982f92927b983f8f912c43f27cf16776c56c
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-04 00:10:53 +0530

    update

--- recursion_again/fibbonacii.cpp
+++ recursion_again/fibbonacii.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+int fibbonacci(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return (num - 1) + (num - 2);
+}
 int main()
 {
-
+    cout << fibbonacci(1) << endl;
     return 0;
 }
