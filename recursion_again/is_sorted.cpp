bae97987100e18858a932912668c9c1c96e11302
SAHIL DUHAN <sahilduhan16092001@gmail.com>
2021-05-04 00:19:18 +0530

    git update

--- recursion_again/is_sorted.cpp
+++ recursion_again/is_sorted.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool is_sorted(int *arr, int size)
+{
+    if (size == 0 || size == 1)
+    {
+        return true;
+    }
+    if (arr[0] > arr[1])
+    {
+        return false;
+    }
+    bool is_smaller = is_sorted(arr + 1, size - 1);
+    return is_smaller;
+}
 int main()
 {
-    
-
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    is_sorted(arr, size);
     return 0;
 }
