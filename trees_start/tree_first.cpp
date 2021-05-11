242fe5fe1f2a4cc722819ff0dcfe77a3ea5fa0d5
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-11 23:47:24 +0530

    git update

--- trees_start/tree_first.cpp
+++ trees_start/tree_first.cpp
@@ -1,13 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-class node
+template <typename T>
+class tree_node
 {
-    int data;
-    node *right;
-    node *left;
+public:
+    T data;
+    vector<tree_node *> children;
+    tree_node(T data)
+    {
+        this->data = data;
+    }
 };
+void print_tree(tree_node<int> *root)
+{
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        print_tree(root->children[i]);
+    }
+}
+
 int main()
 {
-
+    tree_node<int> *root = new tree_node<int>(1);
+    tree_node<int> *node_1 = new tree_node<int>(2);
+    tree_node<int> *node_2 = new tree_node<int>(3);
+    root->children.push_back(node_1);
+    root->children.push_back(node_2);
+    print_tree(root);
     return 0;
 }
