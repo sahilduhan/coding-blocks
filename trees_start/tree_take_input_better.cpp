23fbb4fc056bb85dd8532aa636b14076ecdd1e91
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-13 00:38:03 +0530

    git update

--- trees_start/tree_take_input_better.cpp
+++ trees_start/tree_take_input_better.cpp
@@ -6,17 +6,34 @@ class tree_node
 public:
     T data;
     vector<tree_node *> children;
-    node(T data)
+    tree_node(T data)
     {
         this->data = data;
     }
 };
-tree_node<int> *take_input()
+tree_node<int> *take_input_better()
 {
-    int root_Data = 0;
-    cout << " enter the data " << endl;
-    tree_node<int> *root = new tree_node<int>(root_Data);
+    int root_data;
+    cout << "enter the data" << endl;
+    cin >> root_data;
+    tree_node<int> *root = new tree_node<int>(root_data);
 }
+// tree_node<int> *take_input()
+// {
+//     int root_data;
+//     cout << "enter the data" << endl;
+//     cin >> root_data;
+//     tree_node<int> *root = new tree_node<int>(root_data);
+//     int num_children;
+//     cout << " enter the children" << endl;
+//     cin >> num_children;
+//     for (int i = 0; i < num_children; i++)
+//     {
+//         tree_node<int> *child = take_input();
+//         root->children.push_back(child);
+//     }
+//     return root;
+// }
 void print_tree(tree_node<int> *root)
 {
     if (root == NULL)
@@ -26,11 +43,20 @@ void print_tree(tree_node<int> *root)
     cout << root->data << ":";
     for (int i = 0; i < root->children.size(); i++)
     {
-        cout << root->children[i]->data << " ";
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        print_tree(root->children[i]);
     }
 }
+
 int main()
 {
+
+    // tree_node<int> *root = take_input();
+    //print_tree(root);
 
     return 0;
 }
