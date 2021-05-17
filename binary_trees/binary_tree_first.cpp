dca918d4517ae1180f0ce61ae81e8e098a397ea6
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-17 11:42:17 +0530

    git update

---
+++ binary_trees/binary_tree_first.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Binary_tree_node
+{
+public:
+    int data;
+    Binary_tree_node *right;
+    Binary_tree_node *left;
+
+};
+int main()
+{
+    
+    return 0;
+}
