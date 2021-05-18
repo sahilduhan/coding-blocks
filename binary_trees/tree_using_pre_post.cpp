05730dde560d4fc446484b113f29c7971704d70e
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-18 15:33:56 +0530

    git update

--- binary_trees/tree_using_pre_post.cpp
+++ binary_trees/tree_using_pre_post.cpp
@@ -13,9 +13,15 @@ public:
         left = NULL;
     }
 };
+binary_tree_node *build_tree_help(int in_order, int pre, int in_start, int in_exit, int pre_start, int pre_end)
+{
+}
+binary_tree_node *build_tree(int in_order, int pre_order, int size, int)
+{
+    return build_tree_help(in_order, pre_order, 0, size - 1, 0, size - 1);
+}
 
 int main()
 {
-       
     return 0;
 }
