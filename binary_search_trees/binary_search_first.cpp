9a01bb7dc69f7c051a0c948c3be21627a0d46b76
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-19 23:07:53 +0530

    git upadate

--- binary_search_trees/binary_search_first.cpp
+++ binary_search_trees/binary_search_first.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-class binary_search_tree
+class binary_search_tree_node
 {
-    
+    int data;
+    binary_search_tree_node *left;
+    binary_search_tree_node *right;
+    binary_search_tree_node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
 };
+
 int main()
 {
 
