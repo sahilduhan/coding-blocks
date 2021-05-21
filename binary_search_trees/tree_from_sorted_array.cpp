611b2eb9cfa8491cf794624f12b94a6808e52d80
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-21 23:27:30 +0530

    git update

---
+++ binary_search_trees/tree_from_sorted_array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class binary_search_tree_node
+{
+public:
+    int data;
+    binary_search_tree_node *left;
+    binary_search_tree_node *right;
+    binary_search_tree_node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main()
+{
+
+    return 0;
+}
