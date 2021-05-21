745dccf3eaabf6bf374037170e6031ac12bf331c
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-22 00:12:02 +0530

    git update

--- binary_search_trees/tree_from_sorted_array.cpp
+++ binary_search_trees/tree_from_sorted_array.cpp
@@ -13,8 +13,34 @@ public:
         right = NULL;
     }
 };
+binary_search_tree_node *tree_from_array(int *arr, int start, int end)
+{
+    if (start > end)
+    {
+        return NULL;
+    }
+    int middle = (start + end) / 2;
+    binary_search_tree_node *root = new binary_search_tree_node(arr[middle]);
+    root->left = tree_from_array(arr, start, middle - 1);
+    root->right = tree_from_array(arr, middle + 1, end);
+    return root;
+}
+void pre_order(binary_search_tree_node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    pre_order(root->left);
+    cout << root->data << " ";
+    pre_order(root->right);
+}
 int main()
 {
-
+    binary_search_tree_node *root;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int start = 0;
+    int end = size - 1;
     return 0;
 }
