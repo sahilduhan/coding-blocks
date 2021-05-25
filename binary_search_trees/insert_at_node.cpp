169f34495f55b8cd5a760ad805068a38dbbd308a
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-26 00:40:01 +0530

    git update

---
+++ binary_search_trees/insert_at_node.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class binary_search_tree_node
+{
+public:
+    int data;
+    binary_search_tree_node *left;
+    binary_search_tree_node *right;
+    binary_search_tree_node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+void insert(binary_search_tree_node *root, int data)
+{
+    
+}
+int main()
+{
+
+    return 0;
+}
