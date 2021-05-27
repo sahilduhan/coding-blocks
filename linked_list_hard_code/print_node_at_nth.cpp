b35515b8e14919f48cdc76c44dafd2b29fbcc050
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-27 23:34:04 +0530

    git update

--- linked_list_hard_code/print_node_at_nth.cpp
+++ linked_list_hard_code/print_node_at_nth.cpp
@@ -22,12 +22,33 @@ void print_element(linked_list_node *head, int num)
 {
     int count = 0;
     linked_list_node *temp = head;
-    while (count != num)
+    while (temp->next != NULL)
     {
+        if (count == num)
+        {
+            cout << temp->data << " ";
+        }
+        temp = temp->next;
+        count++;
     }
 }
 int main()
 {
+
+    linked_list_node *head = new linked_list_node(100);
+    linked_list_node *second = new linked_list_node(200);
+    linked_list_node *third = new linked_list_node(300);
+    linked_list_node *fourth = new linked_list_node(400);
+    linked_list_node *fifth = new linked_list_node(500);
+    linked_list_node *sixth = new linked_list_node(600);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = NULL;
+    print(head);
+    print_element(head, 3);
 
     return 0;
 }
