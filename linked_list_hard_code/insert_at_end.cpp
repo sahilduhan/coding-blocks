ca4a378ecd8eff7aabf5340e2de81bfb69b006e8
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-27 23:48:22 +0530

    git update

---
+++ linked_list_hard_code/insert_at_end.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+class linked_list_node
+{
+public:
+    int data;
+    linked_list_node *next;
+    linked_list_node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+void print(linked_list_node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    cout << head->data << " ";
+    print(head->next);
+}
+void insert_at_end(linked_list_node *head, int data)
+{
+    int count = 0;
+    linked_list_node *new_node = new linked_list_node(data);
+    linked_list_node *temp = head;
+    while (temp->next != NULL)
+    {
+        if (temp->next == NULL)
+        {
+            linked_list_node *a = new_node;
+            temp->next = new_node;
+            new_node->next = NULL;
+        }
+        temp = temp->next;
+    }
+}
+int main()
+{
+
+    linked_list_node *head = new linked_list_node(100);
+    linked_list_node *second = new linked_list_node(200);
+    linked_list_node *third = new linked_list_node(300);
+    linked_list_node *fourth = new linked_list_node(400);
+    linked_list_node *fifth = new linked_list_node(500);
+    linked_list_node *sixth = new linked_list_node(600);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = NULL;
+    insert_at_end(head, 1000);
+    print(head);
+
+    return 0;
+}
