e1c6ed56c558850c12d9dae17c479667e83e882f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-05-30 00:08:01 +0530

    git update

---
+++ hash_maps/removing_string_duplicates.cpp
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// vector<int> remove_duplicates(string *arr, int size)
+// {
+//     vector<string> output;
+//     unordered_map<string, bool> seen;
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (seen.count(arr[i]) > 0)
+//         {
+//             continue;
+//         }
+//         seen[arr[i]] = true;
+//         output.push_back(arr[i]);
+//     }
+//     return output;
+// }
+// int main()
+// {
+
+//     return 0;
+// }
