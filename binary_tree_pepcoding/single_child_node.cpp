550b7011e93a175ffed810d2ebcc1142a7f7bc8f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-06 00:57:08 +0530

    git update

--- binary_tree_pepcoding/single_child_node.cpp
+++ binary_tree_pepcoding/single_child_node.cpp
@@ -12,17 +12,29 @@ public:
         left = right = NULL;
     }
 };
-void print_k_node(binary_tree_node *root, int data)
+void print_single_child(binary_tree_node *root)
 {
-    
-
+    if (root == NULL)
+    {
+        return;
+    }
+    if (root->left == NULL && root->right)
+    {
+        cout << root->data << endl;
+    }
+    else if (root->right == NULL && root->left)
+    {
+        cout << root->data << endl;
+    }
+    print_single_child(root->left);
+    print_single_child(root->right);
 }
 int main()
 {
 
     binary_tree_node *root = new binary_tree_node(10);
 
-    //left side of the tree 
+    //left side of the tree
 
     root->left = new binary_tree_node(1);
     root->right = new binary_tree_node(2);
@@ -38,5 +50,6 @@ int main()
     root->right->right = new binary_tree_node(6);
     root->right->right->left = new binary_tree_node(11);
     root->right->right->right = new binary_tree_node(12);
+    print_single_child(root);
     return 0;
 }
