fe45c816e88bcdbef63e193ecff000a472c32a4e
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-15 21:56:20 +0530

    dsg

---
+++ pointer_coding_blocks/array_pointer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int *p)
+{
+    cout << *p << endl;
+}
+void pass_by_value(int *x)
+{
+    x = x + 1;
+}
+void pass_by_reference(int *edn)
+{
+    (*edn)++;
+    cout << edn << endl;
+}
+int main()
+{
+    int p = 10;
+    int *xptr = &p;
+    print(xptr);
+    cout << p << endl;
+    pass_by_value(xptr); // the function pass by value will create a copy ans edit it in the
+    // memory and the edit it but the value of main will be printed
+    // therefore there will be no change in tha value of the ans
+    pass_by_reference(xptr);
+    return 0;
+}
