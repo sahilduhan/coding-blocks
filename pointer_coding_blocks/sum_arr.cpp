4e58e4c14e23fd9ab7b82f777909210c0cec4508
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-15 22:10:14 +0530

    db

---
+++ pointer_coding_blocks/sum_arr.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int *arr, int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << sum(arr, size);
+    return 0;
+}
