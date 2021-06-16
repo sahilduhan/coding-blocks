58de38529765cfdf6372bda455863a8d644a2ce7
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-17 00:19:08 +0530

    azdga

---
+++ dynamic_memory_.cpp/inline.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+int main()
+{
+    int num_1 = 5;
+    int num_2 = 10;
+    int c = max(num_1, num_2);
+    cout << c << endl;
+
+    return 0;
+}
