c1557ac2e42fdc74c417c36bbbf8ebee49397363
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-16 22:32:41 +0530

    adljfn

---
+++ dynamic_memory_.cpp/array_max_dynamic.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout << " enter the size:" << endl;
+    cin >> num;
+    int *arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        cout << " enter the " << i << " element" << endl;
+        cin >> arr[i];
+    }
+    int max = INT_MIN;
+    for (int j = 0; j < num; j++)
+    {
+        if (arr[j] > max)
+        {
+            max = arr[j];
+        }
+    }
+    cout << " the maximum is " << max << endl;
+
+    return 0;
+}
