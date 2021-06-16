8bebfa2bc93c7c23ae4a953681c745ee4b615bc7
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-16 20:59:11 +0530

    git update

--- dynamic_memory_.cpp/array_dynamic.cpp
+++ dynamic_memory_.cpp/array_dynamic.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
-    
+    int *arr = new int[1, 2, 3, 4, 5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
