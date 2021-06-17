f6a5bcd1ecb5789307a4225b20cfa0d27dd7cb97
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-17 15:41:22 +0530

    git update

---
+++ dynamic_memory_.cpp/cosnt_variable.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    const int a = 120;
+    // int &d = a;  since the path of the variable is const and
+    // and cannot be changed therefore it is not possible to create
+    // another variable having the read and write acess
+
+    // of the const variable
+
+    return 0;
+}
+ 
