e6c4ead067c1e3fc519d7b6c8d84dc1cc49b4212
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-18 23:41:06 +0530

    git update

--- oops/constructors.cpp
+++ oops/constructors.cpp
@@ -1,5 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    int age;
+    int roll_num;
+    void display()
+    {
+        cout << "the age and the roll number of the student is " << age << " " << roll_num << endl;
+    }
+    Student()
+    {
+        cout << "no args constructor called" << endl;
+    }
+    void set_age(int value, int number)
+    {
+        age = value;
+        roll_num = number;
+        cout << "the age and  roll number is " << age << " " << roll_num << endl;
+    }
+};
+
 int main()
 {
 
