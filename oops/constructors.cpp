2d4d1758dd64667c26384b7d5a1e68d5c70fcaf3
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-18 23:51:37 +0530

    xzh

--- oops/constructors.cpp
+++ oops/constructors.cpp
@@ -5,13 +5,13 @@ class Student
 public:
     int age;
     int roll_num;
-    void display()
-    {
-        cout << "the age and the roll number of the student is " << age << " " << roll_num << endl;
-    }
     Student()
     {
         cout << "no args constructor called" << endl;
+    }
+    Student(int age, int roll_num)
+    {
+        cout << "constructor with two args is called" << endl;
     }
     void set_age(int value, int number)
     {
@@ -20,9 +20,11 @@ public:
         cout << "the age and  roll number is " << age << " " << roll_num << endl;
     }
 };
-
 int main()
 {
+    Student sahil;
+    sahil.set_age(19, 12);
+    Student duhan(23, 15);
 
     return 0;
 }
