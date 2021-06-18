209aad7a09bd7e02678a26ff8243162e4122bf64
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-19 00:58:40 +0530

    ag

---
+++ oops/fraction_class.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class FractionClass
+{
+public:
+    int numerator;
+    int denominator;
+    FractionClass(int numerator, int denominator)
+    {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+};
+int main()
+{
+
+    return 0;
+}
