c47069cf43847302ee5eeb06511015c529449f6d
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-21 01:05:47 +0530

    bull shit

---
+++ linked_list_good_question/middle.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+void print(Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    cout << head->data << " ";
+    print(head->next);
+}
+int middle(Node *head)
+{
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow->data;
+}
+int main()
+{
+
+    Node *head = new Node(100);
+    Node *second = new Node(200);
+    Node *third = new Node(300);
+    Node *fourth = new Node(400);
+    Node *fifth = new Node(500);
+    Node *sixth = new Node(600);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+
+    return 0;
+}
