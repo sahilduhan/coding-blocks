f89c9edceeb3774b0356ad8415c6685f35ff7421
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-26 00:25:25 +0530

    git update

--- hash_maps_again/first_map.cpp
+++ hash_maps_again/first_map.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int main()
 {
+    unordered_map<string, int> our_map;
+    our_map["shail"] = 1;
+    our_map["duhan"] = 2;
+    our_map["is"] = 3;
+    our_map["a"] = 4;
+    our_map["king"] = 5;
+    cout << our_map["king"] << endl;
+    cout << our_map.at("is") << endl;
 
+    // this map will insert the value
+
+    cout << our_map["king"] << endl;
+
+    // this can be easily used to to test weather a given value is present
+
+    if (our_map.count("king") > 0)
+    {
+        cout << "the val is already present" << endl;
+    }
+
+    // erase function is also present in the hash map STL
+
+    our_map.erase("king");
+
+    // the out put will present a 0 at the value of the nunber where the
+    // value was inserted
+
+    if (our_map.count("king") > 0)
+    {
+        cout << "the val is already present" << endl;
+    }
+    cout<<our_map.size();
     return 0;
 }
