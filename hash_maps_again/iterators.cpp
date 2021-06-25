6f5a6a28b704e9f3ee0ab5cd1713cc1ddc72bbe1
Sahil Duhan <duhan.sahil16@gmail.com>
2021-06-26 00:46:27 +0530

    git

---
+++ hash_maps_again/iterators.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unordered_map<string, int> our_map;
+    our_map["shail"] = 1;
+    our_map["duhan"] = 2;
+    our_map["is"] = 3;
+    our_map["a"] = 4;
+    our_map["king"] = 5;
+    unordered_map<string, int>::iterator it = our_map.begin();
+    while (it != our_map.end())
+    {
+        cout << "value:" << it->first << " and key is: " << it->second << endl;
+        it++;
+    }
+
+    return 0;
+}
