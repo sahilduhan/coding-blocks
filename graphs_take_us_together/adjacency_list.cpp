86796145ed71266b696794c55acd709fababa6ac
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-26 00:51:53 +0530

    git update

---
+++ graphs_take_us_together/adjacency_list.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int node, edges;
+    cin >> node >> edges;
+    vector<int> adj[node + 1];
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    return 0;
+}
---
+++ graphs_take_us_together/adjacency_list_weighted.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int node, edges;
+    cin >> node >> edges;
+    vector<pair<int, int>> adj[node + 1];
+    // a pair is required for
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v, w;
+        cin >> u >> v, w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+}
