b632e61dbaccf392872a09701f6f410791b7ae9f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-29 00:24:23 +0530

    git dupate

--- stacks_and_ques/nearest_greater_to_left.cpp
+++ stacks_and_ques/nearest_greater_to_left.cpp
@@ -6,7 +6,10 @@ void nearest_greater_to_left(int *arr, int size)
     vector<int> final_ans;
     for (int i = 0; i < size; i++)
     {
+        if(ans.size()==0) final_ans.push_back(-1);
+        else if(ans.size()> 0 && ans.top()>arr[i]) final_ans.push_back(ans.top());
         
+        ans.push(arr[i]);
     }
 }
 int main()
