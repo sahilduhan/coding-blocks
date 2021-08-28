2dfb5c5226425baaa8e72797c1bb4bfd6d8140ef
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-29 00:19:58 +0530

    ig tadkv

---
+++ stacks_and_ques/nearest_greater_to_left.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void nearest_greater_to_left(int *arr, int size)
+{
+    stack<int> ans;
+    vector<int> final_ans;
+    for (int i = 0; i < size; i++)
+    {
+        
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    nearest_greater_to_left(arr, size);
+
+    return 0;
+}
