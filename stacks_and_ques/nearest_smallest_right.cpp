538c9bd20e0469db007afcaa2262a10cd5815b31
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-29 12:41:01 +0530

    git update

--- stacks_and_ques/nearest_smallest_right.cpp
+++ stacks_and_ques/nearest_smallest_right.cpp
@@ -7,16 +7,31 @@ void nearest_greater_right(int *arr, int size)
     for (int i = size - 1; i >= 0; i--)
     {
         if (ans.size() == 0) final_ans.push_back(-1);
-        else if (ans.size() > 0 && ans.top()> arr[i]) final_ans.push_back(ans.top());
+        else if (ans.size() > 0 && ans.top() > arr[i]) final_ans.push_back(ans.top());
+
         ans.push(arr[i]);
     }
     reverse(final_ans.begin(), final_ans.end());
     for (int i = 0; i < final_ans.size(); i++) cout << final_ans[i] << " ";
 }
+void near_right(int *arr, int size)
+{
+    vector<int> ans;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[i]) ans.push_back(arr[j]);
+            else ans.push_back(-1);
+        }
+    }
+    for (int i = 0; i < ans.size(); i++) cout << ans[i] << " ";
+}// this is not a good approach O(n^2)
 int main()
 {
-    int arr[] = {};
+    int arr[] = {4, 1, 0, 10, 8};
     int size = sizeof(arr) / sizeof(arr[0]);
     nearest_greater_right(arr, size);
+    // near_right(arr, size);
     return 0;
 }
