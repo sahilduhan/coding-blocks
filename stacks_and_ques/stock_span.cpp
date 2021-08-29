021da980ddb7bc933d22c16b7aafb7cf69ef7e65
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-30 00:18:49 +0530

    fm

--- stacks_and_ques/stock_span.cpp
+++ stacks_and_ques/stock_span.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+void stock_spans(int *arr, int size)
+{
+    
+}
 int main()
 {
+    int arr[] = {};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     return 0;
 }
