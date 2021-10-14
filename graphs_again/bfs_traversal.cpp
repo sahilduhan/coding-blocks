77de6b3036b066e3c8802deaeff6f096f9487579
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-14 23:42:46 +0530

    udape

---
+++ graphs_again/bfs_traversal.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> bfsofGraph(int v, vector<int> adj[])
+    {
+        vector<int> bfs;
+        vector<int> visited(v + 1, 0);
+
+        return bfs;
+    }
+};
+int main()
+{
+
+    return 0;
+}
