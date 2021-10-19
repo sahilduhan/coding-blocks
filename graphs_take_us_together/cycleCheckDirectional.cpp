d85dc78aa22e5322f65b7403d90fc4944ee5a685
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-20 01:05:45 +0530

    git dyae

---
+++ graphs_take_us_together/cycleCheckDirectional.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool checkCycle()
+{
+
+    return false;
+}
+bool isCycle(vector<int> adj_list, int n)
+{
+    
+    return false;
+}
+int main()
+{
+
+    return 0;
+}
