244c9f90439df70cf9bac740a0c2ee0b415f9c20
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-20 00:10:02 +0530

    fsbal

--- graphs_take_us_together/bipartilleGraph.cpp
+++ graphs_take_us_together/bipartilleGraph.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-bool isipartile()
+bool checkBipartile()
 {
 
     return false;
 }
 int main()
 {
-
+    int n, m;
+    cin >> m >> n;
+    vector<int> adj_list[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
     return 0;
 }
