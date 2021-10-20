d7f6126530f77d7b2543d8ad1d6adeedee62d5f3
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-20 23:20:39 +0530

    git update

---
+++ linkedListTakeUForward/deleteNth.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *head)
+    {
+        ListNode *temp = head;
+        while (temp != NULL)
+        {
+            
+        }
+    }
+};
+int main()
+{
+
+    return 0;
+}
