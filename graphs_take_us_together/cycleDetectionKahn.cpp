28c05cd8b0b99f08faf51013f98bf4dff8057be2
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-27 00:26:03 +0530

    updaey

--- graphs_take_us_together/cycleDetectionKahn.cpp
+++ graphs_take_us_together/cycleDetectionKahn.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool isCycle(int node, vector<int> adjList){
+    queue<int> q;
+    vector<int> inDegree(node, 0);
+    for(int i=0;i<node;i++){
+        for(auto it: adjList) inDegree[it]++;
+    }
 
+    for(int i=0;i<node;i++)
+    {
+        if(inDegree[i]==0) q.push(i);
+    }
+
+    int cnt=0;
+    while(q.empty()){
+        int val= q.front();
+        q.pop();
+        cnt++;
+        for(auto it: adjList){
+            inDegree[it]--;
+            if(inDegree[it]==0) q.push(it);
+        }
+
+    }
+    if(cnt==node) return false;
+    return true;
+}
 int main()
 {
 
