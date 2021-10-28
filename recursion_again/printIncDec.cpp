7fa451884189033f1568a5e5e47169f4519c3740
Sahil Duhan <duhan.sahil16@gmail.com>
2021-10-28 22:58:06 +0530

    git duapte

---
+++ recursion_again/printIncDec.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int *arr, int size)
+{
+    
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    return 0;
+}
